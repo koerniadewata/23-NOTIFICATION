aa42aeb5aaaf8d9d2338492e6d15eef1b130b0ab
koerniadewata <118599186+koerniadewata@users.noreply.github.com>
2024-03-06 14:59:32 +0700

    03 - Simple Resource

---
+++ app/Http/Controllers/CategoryNilaiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CategoryNilai;
+use App\Http\Requests\StoreCategoryNilaiRequest;
+use App\Http\Requests\UpdateCategoryNilaiRequest;
+
+class CategoryNilaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryNilaiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoryNilai $categoryNilai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoryNilai $categoryNilai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryNilaiRequest $request, CategoryNilai $categoryNilai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoryNilai $categoryNilai)
+    {
+        //
+    }
+}
